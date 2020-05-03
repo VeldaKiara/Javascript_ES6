57e966f348cba116f55d19b44732679bac317128
Velda Kiara <veldakarimi@gmail.com>
2020-05-03 19:05:22 +0300

    destructuring arrays

---
+++ arrays_destructuring.js
@@ -0,0 +1,7 @@
+let [firstName, middleName, lastName] = ['Velda', 'Karimi', 'Kiara'];
+//above is an array of objects. it prints  the names one after the other
+
+lastName = 'Kent';
+
+console.log(firstName, middleName, lastName)
+//output is  Velda Karimi Kent because the last name changed.
