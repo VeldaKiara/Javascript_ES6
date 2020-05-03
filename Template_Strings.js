51a9743122e537abeb67c300241f5fd3449b28f3
Velda Kiara <veldakarimi@gmail.com>
2020-05-03 18:03:28 +0300

    template strings

---
+++ Template_Strings.js
@@ -0,0 +1,12 @@
+let word1 = 'Velda';
+let word2 = 'Kiara';
+let num1 = 6;
+let num2 = 8;
+
+// const fullName = `${word1} ${word2}`; inline strings
+//to print strings on different lines 
+let example = `${word1} 
+${word2}`;
+// console.log(fullName);
+console.log(example)
+//document.getElementById('example').innerText = example;
