64bca220ae05e9cbded0336723f0ca8d68d149dd
Velda Kiara <veldakarimi@gmail.com>
2020-05-04 15:56:34 +0300

    spread operator

---
+++ spread_operator.js
@@ -0,0 +1,5 @@
+let example1 = [1,2,3,4,5,6];
+let example2 = [...example1]; // this code unpacks all the elements in example 1 to 2 and prints them
+example2.push(true); // this code adds true  to the code in example 1
+
+console.log(example2);
