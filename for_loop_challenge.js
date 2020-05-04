93368a8d2370dab52ee33d85b5fd0300e298e0e4
Velda Kiara <veldakarimi@gmail.com>
2020-05-04 15:50:36 +0300

    for loop challenge

---
+++ for_loop_challenge.js
@@ -0,0 +1,8 @@
+let incomes = [62000, 67000, 75000];
+
+for (const income of incomes) {
+   income += 5000; //this does not work because it is not allowed 
+}
+
+console.log(incomes);
+// this code prints the figures as is 
