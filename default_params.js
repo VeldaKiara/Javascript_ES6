90d09598fbd2097fdd04eb09d613e12e57073b64
Velda Kiara <veldakarimi@gmail.com>
2020-05-04 16:40:55 +0300

    default params

---
+++ default_params.js
@@ -0,0 +1,19 @@
+//The default parameter is a way to 
+//set default values for function parameters 
+//a value is no passed in (ie. 
+//it is undefined ). In a function,
+// Ii a parameter is not provided, 
+//then its value becomes undefined .
+// In this case, the default value that 
+//we specify is applied by the compiler
+
+function add(numArray = []) { //default value
+    let total = 0;
+    numArray.forEach((element) => {
+        total += element;
+    });
+    
+    console.log(total);
+}
+
+add();
