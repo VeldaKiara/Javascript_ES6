4ea662066a2c14354d473bd8a8174ad982ef6990
Velda Kiara <veldakarimi@gmail.com>
2020-05-04 16:59:03 +0300

    let and const

--- es6_includes.js
+++ es6_includes.js
@@ -4,4 +4,5 @@ console.log(numArray.indexOf(0))
 // the code above returns -1 because 0 does not exist
 
 
-console.log(numArray.indexOf(2))
+console.log(numArray.includes(2))
+//includes returns boolean values 
---
+++ let & const .js
@@ -0,0 +1,32 @@
+//if (false) {
+   // var example = 5;
+//}
+
+//console.log(example)
+//  this  code presents an error
+/*
+var example;
+
+if (false) {
+    example = 5;
+}
+
+*/
+const example = [];
+example.push(5)
+console.log(example)
+//this adds 5 to the example and prints it.
+// you cannot change the value as sjhown below
+
+const example = [];
+example = 3;
+console.log(example)
+//this presents a syntax  error
+
+//you can modify arrays and objects but 
+//you cannot override
+
+const example = {};
+example.firstName = 'Velda';
+console.log(example)
+//this prints the name
