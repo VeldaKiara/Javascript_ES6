50da34870bf82fc0487dc3612cff177c55e5a661
Velda Kiara <veldakarimi@gmail.com>
2020-05-04 15:44:44 +0300

    for_loop

---
+++ for_loop.js
@@ -0,0 +1,10 @@
+let incomes = [62000, 67000, 75000];
+let total = 0;
+
+for (const income of incomes) {
+    // the code below prints all the incomes in one line
+    console.log(income);
+    total += income;
+}
+
+console.log(total);
